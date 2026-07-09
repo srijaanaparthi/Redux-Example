d261a5c5857cb62ee7678ff1662dcc031a4f251b
srijaanaparthi <srijaanaparthi@gmail.com>
2026-07-09 12:20:39 +0530

    complete redux project checked in network

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchStart,fetchSuccess,fetchFailure } from './store/Slices/studentSlice';
+import { fetchStart,fetchSuccess,fetchFailure,addStudents,deleteStudents,updatedStudents } from './store/Slices/studentSlice';
 function App() {
  const dispatch=useDispatch();
  const students=useSelector((state)=>state.student.students);
  const loading=useSelector((state)=>state.student.loading);
  const error = useSelector((state)=>state.student.error);
+ const [name,setName]=useState();
+ const [university,setUniversity] =useState();
+ const handleSubmit=()=>{
+  if(name === "") return;
+  const newStudent={
+    id:Date.now(),
+    firstName:name,
+    university:university,
+
+  };
+  dispatch(addStudents(newStudent));
+  setName("");
+  setUniversity("");
+ }
 useEffect(() => {
   dispatch(fetchStart());
 
@@ -30,8 +44,30 @@ useEffect(() => {
         <div key={student.id}>
           <h3>{student.firstName}</h3>
           <p>{student.university}</p>
+         <button className='edit'
+          onClick={()=> {
+            const newName=prompt("enter new student",student.firstName );
+            const newUniversity=prompt("eneter the university",student.university);
+            if(newName&&newUniversity){
+              dispatch(updatedStudents({
+                id:student.id,
+                 firstName:newName,
+                university:newUniversity,
+              }));
+            }
+          }}
+         >edit</button>
+         <button className='delete' onClick={()=>dispatch(deleteStudents(student.id))}>delete</button>
         </div>
       ))}
+      <h2>ADD STUDENT</h2>
+       <label>NAME</label>
+          <input type='text' value={name} onChange={(e)=>setName(e.target.value)} required></input>
+          <br></br>
+          <label>UNIVERSITY</label>
+          <input type='text' value={university} onChange={(e)=>setUniversity(e.target.value)} required></input>
+          <br></br>
+          <button onClick={handleSubmit}>Submit</button>
 </div>
   );
 
--- src/store/Slices/studentSlice.js
+++ src/store/Slices/studentSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import AddStudents from "../../Components/AddStudents";
 const initialState={
     students:[],
     loading:false,
@@ -20,7 +21,25 @@ const studentSlice =createSlice({
         state.loading=false;
         state.error=action.payload;
        },
+       addStudents(state,action){
+        state.students.push(action.payload);
+       },
+       deleteStudents(state,action){
+        state.students=state.students.filter(
+            (student)=>student.id!==action.payload
+        );
+       },
+       updatedStudents(state,action){
+        const {id,firstName,university} = action.payload;
+        const student= state.students.find(
+            (student)=>student.id===id
+        );
+        if(student){
+            student.firstName=firstName;
+            student.university=university;
+        }
+       }
     }
 })
-export const { fetchStart,fetchSuccess,fetchFailure} = studentSlice.actions;
+export const { fetchStart,fetchSuccess,fetchFailure,addStudents,deleteStudents,updatedStudents} = studentSlice.actions;
 export default studentSlice.reducer;
